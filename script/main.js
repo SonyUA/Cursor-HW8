b27ed876ffe626ba280f68680e6af22d99d4e09f
SonYUA <pankivluck@gmail.com>
2022-11-17 19:51:17 +0200

    corection#2500))

--- script/main.js
+++ script/main.js
@@ -5,6 +5,7 @@ class Student {
         this.fullName = fullName;
         this.rating = rating;
         this.arrRating = rating;
+        this.turnOff = true;
     }
 
     getInfo() {
@@ -12,42 +13,52 @@ class Student {
     }
 
     get marks() {
-        return this.rating;
+        if (this.turnOff) {
+            return this.rating;
+        } else {
+            return null;
+        }
     }
 
+    /*все гуд, але краще для функції
+    dismiss і recover використовувати зміну яка відповідає 
+    саме за навчання чи відрахування студента
+    також не зрозуміло чому в set marks має попасти 
+     тру, якщо маєм передавати лише значення типу намбер */
+
     set marks(value) {
-        if (value === null) {
-            this.rating = null;
-        } else if (value === true) {
-            this.rating = this.arrRating;
-        } else if (value !== null && this.rating !== null) {
+        if (Number.isInteger(value) && this.turnOff) {
             return this.rating.push(value);
         } else {
-            return (this.rating = null);
+            return null;
         }
     }
 
     getAverageMark() {
         let result;
-        result = this.rating.reduce(function (acc, item) {
-            return acc + item;
-        }, 0);
-        return result / this.rating.length;
+        if (this.turnOff) {
+            result = this.rating.reduce(function (acc, item) {
+                return acc + item;
+            }, 0);
+            return (result / this.rating.length).toFixed(1);
+        } else {
+            return null;
+        }
     }
 
     dismiss() {
-        return (this.marks = null);
+        return (this.turnOff = false);
     }
 
     recover() {
-        return (this.marks = true);
+        return (this.turnOff = true);
     }
 }
 let newStudent = new Student("Вища Школа Психотерапії м.Одеса", "Студент 1го курсу", "Остап Родоманський Бендер", [5, 4, 4, 5]);
 
 console.log(newStudent.getInfo());
-// newStudent.marks;
-//newStudent.marks = 5;
+console.log(newStudent.marks);
+newStudent.marks = 5;
 console.log(newStudent.getAverageMark());
-//newStudent.dismiss();
+newStudent.dismiss();
 //newStudent.recover();
